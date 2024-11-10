9abba318e501faa8562426a84e0c18c88ed5edc8
Hanush Patibandla <hancsh.the.1@outlook.com>
2024-11-10 13:22:17 -0700

    NFC reading works; login screen works

---
+++ app/src/main/java/com/example/myapplication/login.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class login extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
